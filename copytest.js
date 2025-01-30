1afc2a70cd7a209de2184e4e7099d174df9049d8
Hubble12 <vargadominik@turr.hu>
2025-01-30 08:31:29 +0100

    kepek aranya beallitasa 01

--- copytest.js
+++ copytest.js
@@ -1,7 +1,7 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    
     /*ITT GOMBOT eltüntetni.*/ 
   }
   
